515ac4434bad6c9050484f35aa48d09f3c7539eb
princek <princesamuelpks@gmail>
2025-09-09 10:12:04 +0000

    Update models.py

--- notifications/models.py
+++ notifications/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 class FCMDevice(models.Model):
+
     @property
     def user_model(self):
         return get_user_model()
+    
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
     token = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
