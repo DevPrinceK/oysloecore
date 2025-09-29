f988c16d8eee8b8f9a51331bf7707ab2a0a7968e
princek <princesamuelpks@gmail>
2025-09-29 09:04:18 +0000

    Fix TempChatConsumer import and email regex in routing
    Corrects the import name from TempChatConsumer to TemChatConsumer and updates the user_email regex to allow typical email characters in the WebSocket URL pattern.

--- apiv1/routing.py
+++ apiv1/routing.py
@@ -1,10 +1,11 @@
 from django.urls import re_path
 
-from apiv1.consumers import ChatRoomsConsumer, NewChatConsumer, TempChatConsumer, UnreadCountConsumer
+from apiv1.consumers import ChatRoomsConsumer, NewChatConsumer, TemChatConsumer, UnreadCountConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<room_name>\w+)/$', NewChatConsumer.as_asgi()),
-    re_path(r'ws/tempchat/(?P<user_email>\w+)/$', TempChatConsumer.as_asgi()),
+    # allow typical email characters until next slash
+    re_path(r'ws/tempchat/(?P<user_email>[^/]+)/$', TemChatConsumer.as_asgi()),
     re_path(r'ws/chatrooms/$', ChatRoomsConsumer.as_asgi()),
     re_path(r'ws/unread_count/$', UnreadCountConsumer.as_asgi()),
 ]
