745d1bac82393de806a14bb7b6c1c58da0bdc515
princek <princesamuelpks@gmail>
2025-09-30 09:09:49 +0000

    Update ping.py

--- apiv1/views/ping.py
+++ apiv1/views/ping.py
@@ -7,13 +7,9 @@ from drf_spectacular.utils import extend_schema
 from apiv1.serializers import PingResponseSerializer
 
 
-@extend_schema(
-    methods=['GET'],
-    responses={200: PingResponseSerializer},
-    operation_id='ping'
-)
 class PingAPI(APIView):
     '''This view is used to check if the server is up and running'''
+    @extend_schema(responses={200: PingResponseSerializer}, operation_id='ping')
     def get(self, request):
         '''This method is used to check if the server is up and running'''
         return Response({'message': 'pong'}, status=status.HTTP_200_OK)
