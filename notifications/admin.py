bc9dc3c21ff727cbc3f7d3205e2507773f24c18b
princek <princesamuelpks@gmail>
2025-11-06 23:34:16 +0000

    Update admin.py

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import FCMDevice
+from .models import FCMDevice, Alert
 
 
 @admin.register(FCMDevice)
@@ -7,4 +7,14 @@ class FCMDeviceAdmin(admin.ModelAdmin):
 	list_display = ('id', 'user', 'token', 'created_at')
 	search_fields = ('user__email', 'user__name', 'token')
 	list_filter = ('created_at',)
+	ordering = ('-created_at',)
+
+
+@admin.register(Alert)
+class AlertAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user', 'title', 'kind', 'is_read', 'created_at')
+	search_fields = (
+		'user__email', 'user__name', 'title', 'body'
+	)
+	list_filter = ('is_read', 'kind', 'created_at')
 	ordering = ('-created_at',)
