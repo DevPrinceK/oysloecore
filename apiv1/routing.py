ff70bf35094fef3c94c36fb862d243dcb9b01a65
princek <princesamuelpks@gmail>
2025-12-19 16:13:22 +0000

    update

--- apiv1/routing.py
+++ apiv1/routing.py
@@ -3,7 +3,9 @@ from django.urls import re_path
 from apiv1.consumers import ChatRoomsConsumer, NewChatConsumer, TemChatConsumer, UnreadCountConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', NewChatConsumer.as_asgi()),
+    # Accept any identifier up to the next slash so clients can use room_id values
+    # that include hyphens or other URL-safe characters.
+    re_path(r'ws/chat/(?P<room_name>[^/]+)/$', NewChatConsumer.as_asgi()),
     # allow typical email characters until next slash
     re_path(r'ws/tempchat/(?P<user_email>[^/]+)/$', TemChatConsumer.as_asgi()),
     re_path(r'ws/chatrooms/$', ChatRoomsConsumer.as_asgi()),
